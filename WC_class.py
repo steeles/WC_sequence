1c23c9e646d0b260cfcda289030fadca983c800b
Sara Steele <sara.steele14@gmail.com>
2015-02-10 20:33:21 -0500

    sort of have a class working... also some testing

---
+++ WC_class.py
@@ -0,0 +1,25 @@
+# basic bare bones WC unit
+# this guy wants to know its parameters and how to update its value
+#from numpy import exp
+#print "exp loaded"
+#import numpy as np
+
+class WC_unit:
+
+	nUnits = 0
+	def __init__(self,**kwargs):
+		self.__dict__.update(dict(
+				ke=.1,the=.2,
+				gee = .6,
+				tau=100.,dt=.1,r=0.,Iapp=1.), **kwargs)
+		WC_unit.nUnits +=1
+
+	def update(self):
+		def f(x,k=self.ke,th=self.the):
+			return 1/(1+np.exp(-(x-th)/k))
+		x = self.Iapp + self.gee * self.r
+		self.r += self.dt/self.tau * (-self.r + f(x))
+	
+	# @class_method
+	# def integrator(func,dt,T):
+	# 	tax=
