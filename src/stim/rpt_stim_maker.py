8d09920d46d4caab07b91d0583c6abb4ee0bb5d6
Sara Steele <sara.steele14@gmail.com>
2018-10-13 11:52:01 -0400

    dev done, now tests

--- src/stim/rpt_stim_maker.py
+++ src/stim/rpt_stim_maker.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-from src.stim.stim_maker import fq_tuning_curve, aba_triplet
+from src.stim.stim_maker import fq_tuning_curve # , aba_triplet
 from src.simulation.simulation import TimeAxis
+
 
 class ABAStimulus(TimeAxis):
     """ master stimulus class; subclass of TimeAxis, with time granularity functions """
@@ -25,29 +26,44 @@ class ABAStimulus(TimeAxis):
         self.a_semitone = a_semitone
         self.b_semitone = b_semitone
         self.tone_length = tone_length
-        self.stim_length = len(self.tax)
+        # can it use its own method to create an attribute on init?
+        self.trt = int(self.iti / self.dt)
+        self.period = 4 * self.trt
+        self.tone_time = int(self.tone_length / self.dt)
+        self.tones = self.repeating_tones()
         # we also have ttot, tax
 
     def generate_triplet_tones(self):
-        """ returns 1d numpy.array with the tones """
-        trt = int(self.iti / self.dt)
-        period = 4 * trt
-        tone_time = int(self.tone_length / self.dt)
-
-        triplet = np.zeros(period)
-        triplet[:tone_time] = self.a_semitone
-        triplet[trt:trt + tone_time] = self.b_semitone
-        triplet[2 * trt:2 * trt + tone_time] = self.a_semitone
+        """ returns 1d numpy.array with the tones in a single triplet """
+        triplet = np.zeros(self.period)
+        triplet[:self.tone_time] = self.a_semitone
+        triplet[self.trt:self.trt + self.tone_time] = self.b_semitone
+        triplet[2 * self.trt:2 * self.trt + self.tone_time] = self.a_semitone
         return triplet
 
     def repeating_tones(self):
-        stim_length = self.ttot
+        """
+        create sequence of tones (unit: melopy.key, semitone) on a time grid of dt
+        Returns:
+            numpy.array: 1D array of tones
+        """
         triplet = self.generate_triplet_tones()
-        period=len(triplet)
-        stim = np.zeros(stim_length)
-        n_cycles = int(np.ceil(stim_length/period))
+        tones = np.zeros(self.ttot)
+        n_cycles = int(np.ceil(self.ttot/self.period))
         tmp = np.tile(triplet, [1, n_cycles])
-        stim[:, :tmp.shape[1]] = tmp
+        tones[:, :tmp.shape[1]] = tmp
+        return tones
+
+    def map_tones_to_cell(self, unit):
+        """
+
+        Args:
+            unit (SensoryWCUnit): has attr BF and spread
+        Result:
+            Use the sensory unit's tuning properties to find the input current through their tuning curve
+
+        """
+        pass
 
     def tuning_curve(self, **kwargs):
         """
@@ -60,31 +76,6 @@ class ABAStimulus(TimeAxis):
                 func (callable): we're using normpdf; we'll expect center and spread to be obvious
                 bPlot (bool): whether or not to plot
             Returns:
-
-
-
-# def rpt_stim_maker(e):
-#     tone_length = .030
-#     # exc_gain = 300
-#     tax = arange(0, T, dt)  # s
-#     stim_length = len(tax)
-#     fq_axis = 440 * (2. ** (1 / 12.)) ** arange(-12, 13)
-#     tuning_curve = normpdf(linspace(-7, 7, len(fq_axis)), 0, 3)
-#     tuning_curve /= max(tuning_curve)  # rescale to equal 1
-#     tone_length = int(tone_length / dt)
-#     # df = 5
-#     TRT = int(ITI / dt)
-#     stim = zeros((nUnits, stim_length))
-#
-#     A_ind = 12;
-#     B_ind = A_ind - df
-#     # pdb.set_trace()
-#     stim[0, :tone_length] = tuning_curve[A_ind]
-#     stim[1, TRT:TRT + tone_length] = tuning_curve[B_ind]
-#     stim[2, 2 * TRT:2 * TRT + tone_length] = tuning_curve[A_ind]
-#
-#     period = 4 * TRT
-#     ncycles = int(ceil(stim_length / period))
-#     triplet = stim[:, :period]
-#     tmp = tile(triplet, [1, ncycles])
-#     stim[:, :tmp.shape[1]] = tmp
+                OrderedDict: {semitone: response}
+        """
+        return fq_tuning_curve(**kwargs)
