it turns out i was actually doing the whole syn current calculation wrong - pointwise product, not outer product

src/a_wilson_cowan/synaptic_network.py
@@ -64,13 +64,10 @@ class SynapticNetwork(TonotopicNetwork, Simulation):
         self.kSs = np.array([x.kS for x in units_array])
         self.thSs = np.array([x.thS for x in units_array])
         self.f_S = self.f_activation_builder(self.kSs, self.thSs, self.b_00)
-        # initialize
-        self.point_wise_Isyn = np.outer(self.syn_weights, self.R_var_array)
 
     def run(self):
         while self.t_i < self.ttot-1:
             self.update_all()
-        self.point_wise_Isyn = np.outer(self.syn_weights, self.R_var_array)
 
     def update_all(self, n=1):
         """ we want to calculate all the deltas first then we can add them """
@@ -152,12 +149,9 @@ class SynapticNetwork(TonotopicNetwork, Simulation):
         unit_dict["stim"] = self.stim_currents[unit_ind] * self.gstims[unit_ind]
         unit_dict["SFA"] = self.A_var_array[unit_ind] * abs(self.gSFAs[unit_ind]) * -1
         unit_dict["rec_exc"] = self.R_var_array[unit_ind] * self.gees[unit_ind]
-        syn_ind = n_units * unit_ind
-        syn_currents = self.point_wise_Isyn[syn_ind:syn_ind + n_units, :self.ttot] #[:self.ttot]
+        syn_currents = (self.R_var_array.transpose() * self.syn_weights[unit_ind]).transpose()
         for s_ind in xrange(syn_currents.shape[0]):
-            print s_ind
             syn = syn_currents[s_ind, :]
-            print(max(syn))
             if any(syn):
                 unit_dict["Isyn_u{}".format(s_ind)] = syn
         return unit_dict
@@ -171,7 +165,7 @@ class SynapticNetwork(TonotopicNetwork, Simulation):
             ndf["unit"] = "u{}".format(ind)
             unit_dfs.append(ndf)
         # TODO: make sure this doesn't clobber columns when u0, 1, etc get involved... i think it shouldn't...
-        return pd.concat(unit_dfs)
+        return pd.concat(unit_dfs) # .fillna(0)
 
     def get_dR_R(self, unit_ind, Iapp=None, bPlot=True):
         """
@@ -187,7 +181,7 @@ class SynapticNetwork(TonotopicNetwork, Simulation):
             Iapp = self.i_0s[unit_ind]
         tau = self.taus[unit_ind]
         gee = self.gees[unit_ind]
-        fe = self.f_activation_builder(self.kes[unit_ind], self.thes[unit_ind])
+        fe = self.f_activation_builder(self.kes[unit_ind], self.thes[unit_ind], b_00=self.b_00)
         r_arr = np.linspace(-.1, 1., 1000)
         dR_arr = self.delta_R(r_arr, Iapp=Iapp, tau=tau, fe=fe, dt=self.dt*1000, gee=gee)
         out = pd.DataFrame({"R_ax": r_arr, "dR": dR_arr})

src/simulation/synaptic_sim.py
@@ -17,7 +17,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from src.sim_plots.make_figures import generic_plot
-from src.sim_plots.sns_plots import plot_more_generic_traces
+# from src.sim_plots.sns_plots import plot_more_generic_traces
 from src.stim.stimulus import ABAStimulus
 from src.a_wilson_cowan.sensory_network import SensoryWCUnit, TonotopicNetwork, Selectivity
 from src.simulation.simulation import Simulation
@@ -26,12 +26,15 @@ from src.stim.intervals import Intervals
 
 plt.close()
 
-i_0 = -.05
+sensory_pars = dict(i_0 = -.05, gee = 0.5, gSFA = 0.5)
+integration_pars = dict(i_0 = -0.05, tau=50, gee=0.3, SFA = 0.3, the = 0.4)
+
+
 pars_list = [
-    {"gee": 0.5, "i_0": i_0},
-{"gee": 0.1, "i_0": i_0}, {"gee": 0.5, "i_0": i_0, "the": 0.4}
+    sensory_pars,
+sensory_pars, integration_pars
 ]
-T = 3.
+T = 1.
 
 # from micheyl
 stim = Intervals(T=T, ITI=125, tone_length=125)
@@ -43,16 +46,24 @@ s_units = [
 ]
 
 weights = np.array([
-    [0, 0, 0], [0, 0, 0], [.39, .39, 0]
+    [0, 0, 0], [0, 0, 0], [.35, .15, 0]
 ])
 
-network = SynapticNetwork(pars_list=pars_list, selectivities=s_units, stimulus=stim, syn_weights=weights, T=T)
+network = SynapticNetwork(pars_list=pars_list, selectivities=s_units, stimulus=stim, syn_weights=weights, T=T, b_00=True)
 network.run()
 data = network.build_unit_dfs()
-g = sns.FacetGrid(data, col='unit', col_wrap=1)
-g.map_dataframe(plot_more_generic_traces)
+
+to_plot = ["FR", "SFA", 'Isyn_u0', 'Isyn_u1', 'rec_exc', 'stim']
+
+plot_df = data[to_plot + ['tax', 'unit']].melt(
+    id_vars=['tax', 'unit'], var_name = 'name', value_name='response'
+)
+
+g = sns.FacetGrid(data=plot_df, hue='name', col='unit', col_wrap=1)
+g.map_dataframe(plt.plot, "tax", "response")
+g.add_legend()
 plt.show()
 dRR = network.get_dR_R(0, bPlot=False)
 dRR["zline"]=0
-# dRR.plot(x="R_ax")
+dRR.plot(x="R_ax")
 